59f2cdca6a451dab961780f679d505e867cf7eb6
Damian Walsh <damianwalsh@me.com>
2023-01-10 15:19:48 +0000

    First commit updated site

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  content: ["./src/**/*.{html,njk}"],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+    },
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
+      fontFamily: {
+        sans: ['Inter var',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
