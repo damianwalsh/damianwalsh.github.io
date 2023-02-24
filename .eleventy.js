857e9523699f4f73dc2d65858fd6de07a16a62d5
Damian Walsh <damianwalsh@me.com>
2023-02-24 16:23:43 +0000

    Remove BrowserSync

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,7 @@ module.exports = function(eleventyConfig) {
 
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
-  } else {
-    eleventyConfig.setBrowserSyncConfig({ callbacks: { ready: browserSyncReady }});
-
-  }
+  } 
 
   // Passthrough
   eleventyConfig.addPassthroughCopy({ "src/static": "." });
@@ -31,18 +28,6 @@ module.exports = function(eleventyConfig) {
   }
   
 };
-
-function browserSyncReady(err, bs) {
-  bs.addMiddleware("*", (req, res) => {
-    const content_404 = fs.readFileSync('_site/404.html');
-    // Provides the 404 content without redirect.
-    res.write(content_404);
-    // Add 404 http status code in request header.
-    // res.writeHead(404, { "Content-Type": "text/html" });
-    res.writeHead(404);
-    res.end();
-  });
-}
 
 function htmlminTransform(content, outputPath) {
   if( outputPath.endsWith(".html") ) {
