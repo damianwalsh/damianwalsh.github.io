02a258868b7f4d2ee41deb520ee1694914f63f83
Damian Walsh <damianwalsh@me.com>
2025-03-10 14:04:37 +0000

    Remove trailing slash - URL used to construct absolute paths in social sharing images

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
   title: "Damian Walsh",
-  url: "https://damianwalsh.co.uk/",
+  url: "https://damianwalsh.co.uk",
   language: "en",
   description: "Designer (Interaction/UX/UI) based in Manchester, UK",
   author: {
     name: "Damian Walsh",
     email: "damianwalsh@me.com",
-    url: "https://damianwalsh.co.uk/"
+    url: "https://damianwalsh.co.uk"
   }
 }
